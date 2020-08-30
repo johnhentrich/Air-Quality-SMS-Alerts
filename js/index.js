0395583815a034dbe13c57529d6f705278da37b9
chengcj-upenn <chengcj@seas.upenn.edu>
2020-08-29 17:49:02 -0700

    added alert pause function, cleaned up code

--- js/index.js
+++ js/index.js
@@ -1,156 +1,139 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var TWILIO_SID = "AC68e2917af55b208fc43fcb21a6a6bde6";
+  var TWILIO_PHONE = "13057833843";
+  var TWILIO_KEY = "92f0e2372694e9a522cb8b415e5212d0";
+  var count = 0;
+  var lat;
+  var lon;
+  var tempf;
+  var description;
+  var current_aqi;
+  var weather_url;
+  var weatherlink;
+  var current_aqi_url;
+  var body2 = "text message";
+  var location;
+  var phone;
+  var minutes;
+  var text_on = false;
+  var btn = document.getElementById("submit-button");
+  var interval = null;
 
-var count = 0 ;
-var lat;
-var lon;
-var tempf ;
-var tempc ;
-var description ;
-var current_aqi ;
-var weather_url ;
-var weatherlink ;
-var current_aqi_url ;
-var body ;
-var body2 = "test";
-var location ;
-var phone ;
-var minutes ;
-
-
-  $(document).bind("keypress", function(e) {
+  $(document).bind("keypress", function (e) {
     if (e.which === 13) {
-      // return
       $(".formSubmit").trigger("click");
     }
   });
 
-  var aqi_checked = false;
-  var rain_checked = false;
-
+  // get checkbox status for AQI and Weather Description
   var aqi = document.querySelector('input[id="aqi"]');
-  var rain = document.querySelector('input[id="rain"]');
-  
-  aqi.addEventListener('click', updateAQIDisplay);
-  rain.addEventListener('click', updateRainDisplay);
-    function updateAQIDisplay() {
-    aqi_checked = aqi.checked
-    $("#display-aqi-data").html(aqi_checked);
+  var desc = document.querySelector('input[id="desc"]');
+
+  //find coordinates and make url 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+
+      lat = pos.coords.latitude;
+      lon = pos.coords.longitude;
+    });
   }
 
-  function updateRainDisplay() {
-    rain_checked = rain.checked
-    $("#display-rain-data").html(rain_checked);
-  }
+  // after clicked on the button, regularly provide alerts
+  // if clicked on the button again, will pause alerts
+  $(".formSubmit").click(function () {
+    if (!text_on){
+      btn.innerHTML = "Signed up for alerts";
+      text_on = true
 
-//find coordinates and make url 
-if (navigator.geolocation)
-{
-  navigator.geolocation.getCurrentPosition(function(pos) {
+      go()
+      phone = document.getElementsByName("phone")[0].value;
+      minutes = document.getElementsByName("minutes")[0].value;
+      interval = setInterval(go, minutes * 1000 * 60);
 
-    lat  = pos.coords.latitude;
-    lon = pos.coords.longitude;
+    } else {
+      clearInterval(interval);
+      btn.innerHTML = "Alert Paused";
+      text_on = false
+    }    
   });
-}
 
+  // executed at regular intervals to provide update
+  function go() {
+    count++;
+    
+    // get data from openweather api
+    weather_url = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=76aa4cb2e2b53b603516bce13382a600";
 
-function go () {
-  count++
-
-  weather_url = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=imperial&appid=76aa4cb2e2b53b603516bce13382a600";
-
-    //json stuff with url
-    $.getJSON(weather_url,function(data){
-      location = data.name ;
+    $.getJSON(weather_url, function (data) {
+      location = data.name;
       $(".weather").html("Location: " + data.name);
 
       tempf = data.main.temp.toFixed(1) + " degrees fahrenheit"
-      tempc = ((data.main.temp-32)*(5/9)).toFixed(1) + " degrees celsius"
-      $(".weather2").html("Temperature: "+tempf);
+      $(".weather2").html("Temperature: " + tempf);
 
+      //weather description
+      description = data.weather[0].description;
+      $(".weather3").html("Weather description is " + description);
 
- //weather data
- description = data.weather[0].description ;
- $(".weather3").html("Weather description is "+ description);
+      //weather images
+      $(".weather4").html(function () {
+        weatherlink = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        return "<img src=" + weatherlink + ">";
+      });
 
-//weather images
-$(".weather4").html(function() 
-{         weatherlink = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-return "<img src="+ weatherlink +">";
-});
-
-
-
-//end of brackets
-});   
-
-    current_aqi_url = "http://api.airvisual.com/v2/nearest_city?lat="+lat+"&lon="+lon+"&key=71270c59-114c-4a35-b948-025988b30f33" ;
-    
-    $.getJSON(current_aqi_url,function(data){
-      current_aqi = data.data.current.pollution.aqius.toFixed(1) ;
     });
 
-    $(".weather5").html("Current air quality index is "+current_aqi); 
-    $(".weather6").html("Refresh count: "+count); 
-    body = current_aqi ; 
-    body2 = "Current temperature today in " + location+" is " + tempf;
+    // get air quality index from air visual api
+    current_aqi_url = "http://api.airvisual.com/v2/nearest_city?lat=" + lat + "&lon=" + lon + "&key=71270c59-114c-4a35-b948-025988b30f33";
 
-    if (aqi_checked == true) {
-    body2 = body2 + ", "+ "current air quality is "+current_aqi ;
-  }
+    $.getJSON(current_aqi_url, function (data) {
+      current_aqi = data.data.current.pollution.aqius.toFixed(1);
+    });
 
-    if (rain_checked == true) {
-    body2 = body2 + ", " + "weather description is "+description;
-  }
-    if (location===undefined || tempf === undefined || current_aqi === undefined || description === undefined) {
+    $(".weather5").html("Current air quality index is " + current_aqi);
+    $(".weather6").html("Refresh count: " + count);
 
+    // prepare text message
+    body2 = "Current temperature today in " + location + " is " + tempf;
+
+    if (aqi.checked == true) {
+      body2 = body2 + ", " + "current air quality is " + current_aqi;
+    }
+
+    if (desc.checked == true) {
+      body2 = body2 + ", " + "weather description is " + description;
+    }
+
+    // send text messange to user
+    if (location === undefined || tempf === undefined || current_aqi === undefined || description === undefined) {
+      // API sometimes does not respond; pass and don't send text message
     } else {
-          $(".weather7").html("Summary: " +body2);
-    text();
+      $(".weather7").html("Summary: " + body2);
+      text();
     }
   }
 
-
-
-$(".formSubmit").click(function() {
-      phone = document.getElementsByName("phone")[0].value;
-      minutes = document.getElementsByName("minutes")[0].value;
-      setInterval(go,minutes*1000*60) ;
-  });
-
-  //$(".btnSubmit").click(function()
-
+  // send text message using Twilio API
   function text() {
 
-          // Your Twilio credentials
-          var SID = "SID" ;
-          var Key = "KEY" ; 
-
-          $.ajax({
-            type: 'POST',
-            url: 'https://api.twilio.com/2010-04-01/Accounts/' + SID + '/Messages.json',
-            data: {
-              "To" : phone,
-              "From" : "TWILIO PHONE #",
-              "Body" : body2
-            },
-            beforeSend: function (xhr) {
-              xhr.setRequestHeader ("Authorization", "Basic " + btoa(SID + ':' + Key));
-            },
-            success: function(data) {
-              console.log(data);
-            },
-            error: function(data) {
-              console.log(data);
-            }
-          });
-        }
-
-        //);
-
-
-
-
-
-  //setInterval(go,15000) ;
+    $.ajax({
+      type: 'POST',
+      url: 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_SID + '/Messages.json',
+      data: {
+        "To": phone,
+        "From": TWILIO_PHONE,
+        "Body": body2
+      },
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", "Basic " + btoa(TWILIO_SID + ':' + TWILIO_KEY));
+      },
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (data) {
+        console.log(data);
+      }
+    });
+  }
 
 });
